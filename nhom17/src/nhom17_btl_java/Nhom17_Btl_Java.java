a1e4678b7aaafb429b708c2fe93e489b8c5bd7e3
tranhongphi <phitran398@gmail.com>
2020-06-15 23:58:37 +0700

    chua hoan chinh

---
+++ nhom17/src/nhom17_btl_java/Nhom17_Btl_Java.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nhom17_btl_java;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Nhom17_Btl_Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Login();
+    }
+}
